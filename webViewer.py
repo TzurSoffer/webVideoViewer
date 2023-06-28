1a58a9d08cd61f60904d41708c3e9ec0520c35d1
Tzur <103438808+RandomTheFirst@users.noreply.github.com>
2023-06-27 21:10:58 -0700

    rename and minor changes

--- webViewer.py
+++ webViewer.py
@@ -1,60 +1,82 @@
-import cv2
-from flask import Flask, render_template, Response, render_template_string
-import os
+from flask import Flask, Response
 import threading
-import sys
+import cv2
+import os
+import base64
 
-class VideoStream:
-    def __init__(self, port=5000):
+class SubpageManager:
+    def __init__(self):
+        self.frames = {}
+    
+    def imshow(self, name, frame):
+        self.frames[name] = frame
+    
+    def getFrame(self, name):
+        return self.frames.get(name, None)
+
+class SubpageApp:
+    def __init__(self, homePageTemplate="templates/home.html", subpageTemplate="templates/subpage.html", port=80):
         self.currentFolder = os.path.dirname(os.path.abspath(__file__))
         self.app = Flask(__name__, template_folder=self.currentFolder)
 
-
-        @self.app.route('/')
-        def index():
-            return render_template('index.html')
-
-        @self.app.route('/videoFeed')
-        def videoFeed():
-            return Response(self.videoUpdater(""), mimetype='multipart/x-mixed-replace; boundary=frame')
+        self.manager = SubpageManager()
 
         self.port = port
-        self.videos = {}
-        self.running = True
-        self.paused = False
+
+        self.setupRoutes()
+        self.homePageTemplate = homePageTemplate
+        self.subpageTemplate = subpageTemplate
+        self.subpages = []
+        self.homePage = "<h1>Subpages for Names</h1><ul>{}</ul>"
+
+    def setupRoutes(self):
+        self.app.route('/')(self.index)
+        self.app.route('/<name>')(self.subpage)
+        self.app.route('/imshow/<name>')(self.imshow)
+
+    def addSubpage(self, name):
+        self.subpages.append(name)
     
-    def _createTemplate(self, subpage):
-        with open(self.templatePath)
+    def index(self):
+        subpages = ''.join(f'<li><a href="/{name}">{name}</a></li>' for name in self.subpages)
+        return self.homePage.format(subpages)
+    
+    def subpage(self, name):
+        def generate_frames():
+            while True:
+                frame = self.manager.getFrame(name)
+                if frame is None:
+                    yield b'--frame\r\n'
+                    yield b'Content-Type: text/html\r\n\r\n'
+                    yield b'<h1>No subpage found for {name}</h1>\r\n\r\n'
+                else:
+                    yield b'--frame\r\n'
+                    yield b'Content-Type: image/jpeg\r\n\r\n'
+                    yield base64.b64decode(frame)
+
+        return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+    def imshow(self, name, frame):
+        if self.manager.getFrame(name) is None:   # subpage doesn't exist
+            self.addSubpage(name)
+
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = base64.b64encode(buffer).decode('utf-8')
+        self.manager.imshow(name, frame)
     
     def run(self):
         threading.Thread(target=self.app.run, kwargs={"host": "0.0.0.0", "port": self.port}).start()
 
-    def pause(self):
-        self.paused = True
-
-    def unpause(self):
-        self.paused = False
-
-    def videoUpdater(self, subpage):
-        while self.running:
-            while not self.paused and self.running:
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + self.videos[subpage] + b'\r\n')
-
-    def imshow(self, name, frame):
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        self.videos[name] = frame
 
 if __name__ == '__main__':
+    app = SubpageApp()
     cap = cv2.VideoCapture(0)
-    videoStream = VideoStream()
-
-    try:
-        while True:
-            _, frame = cap.read()
-            videoStream.imshow("frame", frame)
-    except Exception as e:
-        print(e)
-        sys.exit()
-        
+    
+    app.run()
+    
+    # Continuously capture frames from the webcam and update subpages
+    while True:
+        _, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        app.imshow("frame", frame)
+        app.imshow("gray", gray)
